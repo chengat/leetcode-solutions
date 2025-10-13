7749390c6631d671222d620a35e88d4d85b7ed95
Nikhil Nambiar Chengat <90534285+chengat@users.noreply.github.com>
2025-10-13 12:24:43 -0400

    Time: 0 ms (100%), Space: 17.8 MB (54.74%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        word = word.lower()
+        print(word)
+        containsVowel = False
+        containsConsonant = False
+        for i in word:
+            #print(i)
+            if (i == "a") or (i == "e") or (i == "i") or (i == "o") or (i == "u"):
+                containsVowel = True
+            elif i.isalpha() == True:
+                containsConsonant = True
+        #print(containsVowel, containsConsonant)
+       
+        if (len(word) >= 3) and (word.isalnum() == True) and (containsVowel == True) and (containsConsonant == True):
+            return True
+        else:
+            return False
+        
